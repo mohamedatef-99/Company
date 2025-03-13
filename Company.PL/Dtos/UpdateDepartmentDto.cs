7347e12b4c5b83d9a2aa9c8611ec4e15e9c8e7be
Mohamed Atef <Mohamed Atef>
2025-03-13 17:41:50 +0200

    Implement View Details and Edit

---
+++ Company.PL/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is Required !")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is Required !")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "CreateAt is Required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
