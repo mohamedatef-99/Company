26c8a0425871c5196f901d188deb4b1e19ea5a90
Mohamed Atef <Mohamed Atef>
2025-03-28 01:21:15 +0200

    Implement SignIn and SignUp

---
+++ Company.PL/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using Company.DAL.Models;
+using Company.PL.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user is null)
+                {
+                    user = await _userManager.FindByEmailAsync(model.Email);
+                    if (user is null)
+                    {
+                        user = new AppUser
+                        {
+                            UserName = model.UserName,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            IsAgree = model.IsAgree
+                        };
+                        var result = await _userManager.CreateAsync(user, model.Password);
+                        if (result.Succeeded)
+                        {
+                            return RedirectToAction("SignIn");
+                        }
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+                ModelState.AddModelError("", "Invalid SignUp");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if(user is not null)
+                {
+                    var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+                    if (flag)
+                    {
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+                ModelState.AddModelError("", "Invalid Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ Company.PL/Dtos/SignUpDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.Dtos
+{
+    public class SignUpDto
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and Confirm Password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsAgree { get; set; }
+    }
+}
