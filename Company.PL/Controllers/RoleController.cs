5907b5685a6ca29d5ef4d147c2d9633989c118e3
Mohamed Atef <Mohamed Atef>
2025-03-29 00:02:22 +0200

    Role Controller and User Controller

---
+++ Company.PL/Controllers/RoleController.cs
@@ -0,0 +1,145 @@
+﻿using Company.DAL.Models;
+using Company.PL.Dtos;
+using Company.PL.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManger;
+
+        public RoleController(RoleManager<IdentityRole> roleManger)
+        {
+            _roleManger = roleManger;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleToReturn model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManger.FindByNameAsync(model.Name);
+                if (role is null)
+                {
+                    role = new IdentityRole()
+                    {
+                        Name = model.Name
+                    };
+                    var result = await _roleManger.CreateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+                return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string? SearchInput)
+        {
+            IEnumerable<RoleToReturn> roles;
+            if (string.IsNullOrEmpty(SearchInput))
+            {
+                roles = _roleManger.Roles.ToList().Select(U => new RoleToReturn()
+                {
+                    Id = U.Id,
+                    Name = U.Name
+                });
+            }
+            else
+            {
+                roles = _roleManger.Roles.ToList().Select(U => new RoleToReturn()
+                {
+                    Id = U.Id,
+                    Name = U.Name
+                }).Where(R => R.Name.ToLower().Contains(SearchInput.ToLower()));
+            }
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string? id, string viewName = "Details")
+        {
+            if (id is null) return BadRequest("Invalid Id");
+            var role = await _roleManger.FindByIdAsync(id);
+            if (role is null) return NotFound(new { statusCode = 404, message = "Role Not Found" });
+
+            var dto = new RoleToReturn()
+            {
+                Id = role.Id,
+                Name = role.Name
+            };
+
+            return View(viewName, dto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string? id)
+        {
+
+            return await Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] string id, RoleToReturn model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest("Invalid Operation");
+                var role = await _roleManger.FindByIdAsync(id);
+                if (role is null) return BadRequest("Invalid Operation");
+
+                var roleResult = await _roleManger.FindByNameAsync(model.Name);
+                if(roleResult is null)
+                {
+                    role.Name = model.Name;
+
+                    var result = await _roleManger.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                ModelState.AddModelError("", "Invalid Operation");
+                
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string? id)
+        {
+            return await Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromRoute] string id, RoleToReturn model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest("Invalid Operation");
+                var role = await _roleManger.FindByIdAsync(id);
+                if (role is null) return BadRequest("Invalid Operation");
+
+                var result = await _roleManger.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ModelState.AddModelError("", "Invalid Operation");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Company.PL/Dtos/RoleToReturn.cs
@@ -0,0 +1,8 @@
+﻿namespace Company.PL.Dtos
+{
+    public class RoleToReturn
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
